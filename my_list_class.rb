c34dbcebb4d4fed9496d7cb33753024db94c5563
otmanTR <r.brk.otman@gmail.com>
2023-04-13 17:54:36 +0300

    update my_list_class.rb

---
+++ my_list_class.rb
@@ -0,0 +1,14 @@
+require_relative 'my_numerable_module'
+
+class MyList
+    include MyEnumerable
+
+    def initialize(*list)
+        @list = list
+    end
+
+    def each(&block)
+        @list.each(&block)
+    end
+end
+
