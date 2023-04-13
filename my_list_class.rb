3ce2e0bd27b2af1d7d0749a0f852f1fb3fd660e7
LeslieAine <aineleslie@gmail.com>
2023-04-13 22:29:13 +0300

    edit class file

--- my_list_class.rb
+++ my_list_class.rb
@@ -1,4 +1,4 @@
-require_relative 'my_numerable_module'
+require './my_numerable_module'
 
 class MyList
   include MyEnumerable
@@ -11,11 +11,3 @@ class MyList
     @list.each(&)
   end
 end
-
-list = MyList.new(1, 2, 3, 4)
-
-puts list.all? { |e| e < 5 }
-puts list.all? { |e| e > 5 }
-puts list.any? { |e| e == 2 }
-puts list.any? { |e| e == 5 }
-puts list.filter(&:even?)
